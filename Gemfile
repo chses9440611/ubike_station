95dece27c61318e82490a3176ea603007c65c892
Jason Huang <chses910372@gmail.com>
2017-03-19 02:15:16 +0800

    enable to get request location info

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
-
+gem 'httparty'
+gem 'crack'
 gem 'json'
 gem 'open_uri_redirections'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/bike_station_controller.rb
+++ app/controllers/bike_station_controller.rb
@@ -1,17 +1,39 @@
 require 'json'
+require 'httparty'
+require 'crack'
 require 'open_uri_redirections'
 class BikeStationController < ApplicationController
 	def index
+		place_id = "place_id=ChIJi73bYWusQjQRgqQGXK260bw"
+		map_uri = "https://maps.googleapis.com/maps/api/geocode/json?"
+		api_key = "AIzaSyBaWsuNSpmcPIOzlfC9oR_R6HXvQ4qFyDo"
+		full_path = "#{map_uri}latlng=#{params["lat"]},#{params["lng"]}&result_type=country|postal_code&key=#{api_key}"
+		#puts full_path
+		#get the ubike data
 		@data = get_data()
-		render json: @data["retVal"]
+		#get the location data
+		geocode = HTTParty::get(full_path)
+		@geodata = Crack::JSON.parse(geocode.body)
+
+
+
+		#the way of show
+		render json: {:info => @data["retVal"], :geo => @geodata}
+
+
+
+		#here is test
 		puts params["lat"]
 		puts params["lng"]
 		#@data["retVal"].each do |key, value|
 		h1 =  @data["retVal"]["0001"]
+		h2= @geodata["results"][0]["address_components"]
 		puts h1
+		puts h2
 		#end
 	end
 private
+	#@api_key = "AIzaSyBaWsuNSpmcPIOzlfC9oR_R6HXvQ4qFyDoyy"
 	def get_data
 		uri = "http://data.taipei/youbike"#dataa uri
 		File.open("bike_data.json", "wb") do |file|
