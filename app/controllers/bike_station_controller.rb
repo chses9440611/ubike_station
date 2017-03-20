c70f458e4d399b6b2875082e0e82b48bbdf49a88
Jason Huang <chses910372@gmail.com>
2017-03-20 21:04:52 +0800

    Add comments

--- app/controllers/bike_station_controller.rb
+++ app/controllers/bike_station_controller.rb
@@ -15,73 +15,67 @@ class BikeStationController < ApplicationController
 			@result = pack(c, @result_arr)						
 		elsif c== -3											#system error
 			@result = pack(c, @result_arr)
-		elsif c == 0 && @geodata.class == @result.class
-			@result_arr =  get_near_station(@data)
+		elsif c == 0 && @geodata.class == @result.class			#OK
+			@result_arr =  get_near_station(@data)				#get two nearest station
 			@result = pack(c, @result_arr)
 		end
 
 		#the way of show
-		render json: @result
+		render json: @result									#render with json
 	end
 
 private
 	def get_data
-		#dataa uri
 		uri = "http://data.taipei/youbike"
-		File.open("bike_data.json", "wb") do |file|
-			#download data as bike_data.json
+		File.open("bike_data.json", "wb") do |file|				#download data as bike_data.json
 			begin
-				file.write open(uri, :allow_redirections => :safe).read
+				file.write open(uri, :allow_redirections => :safe).read 
 			rescue StandardError => e
-				return -3
+				return -3										#download fail => return -3
 			end
 
 		end
 
-		file = File.open("bike_data.json", "r")
+		file = File.open("bike_data.json", "r")					#read file
 		p = file.read
 
-		data = JSON.parse(p)
+		data = JSON.parse(p)									#turn JSON to HASH
 		return data
 	end
 		
 	def get_geo_info
 		la = params["lat"].to_f
 		ln = params["lng"].to_f
-		puts la
-		puts ln
-		if la > 90.0 || la < -90.0 || ln > 180.0 || ln < -180.0
+		if la > 90.0 || la < -90.0 || ln > 180.0 || ln < -180.0	#check the validation of latitude and longitude and return error code:-1
 			message = "Invalid latitude and longtitude"
 			puts message
 			return -1
 		end
 	
-		place_id = "place_id=ChIJi73bYWusQjQRgqQGXK260bw"
-		map_uri = "https://maps.googleapis.com/maps/api/geocode/json?"
-		api_key = "AIzaSyBaWsuNSpmcPIOzlfC9oR_R6HXvQ4qFyDo"
+		map_uri = "https://maps.googleapis.com/maps/api/geocode/json?"	#API main uri
+		api_key = "AIzaSyBaWsuNSpmcPIOzlfC9oR_R6HXvQ4qFyDo"				#API KEY
 		full_path = "#{map_uri}latlng=#{params["lat"]},#{params["lng"]}&result_type=country|postal_code&key=#{api_key}"
 
 		begin
-		get_location = HTTParty::get(full_path)
-		rescue HTTParty::Error => e
+		get_location = HTTParty::get(full_path)							#get the geo-data of params["lat", "lng"]
+		rescue HTTParty::Error => e										#HTTP error => return -3
 			return -3
-		rescue StandardError => e
+		rescue StandardError => e										#system error
 			return -3
 		end
 
-		data = JSON.parse(get_location.body)
-		puts "Hello"
-		if data["results"] == []
-			puts "The location is not Taipei"
+		data = JSON.parse(get_location.body)							#turn geo-data to HASH
+		if data["results"] == []										#empty data => no data about this location
+			puts "The location is not in Taipei"
 			return -2
 		end
-		for x in data["results"][0]["address_components"]
+		for x in data["results"][0]["address_components"]				#find the key-value is admini_level1- City
 			if x["types"][0] == "administrative_area_level_1"
 				h = x
 			end
 		end
 		
-		if h["long_name"] != "Taipei City"
+		if h["long_name"] != "Taipei City"								#check the location is in Taipei City?
 			message = "The location is not in Taipei"
 			puts message
 			return -2
@@ -90,7 +84,7 @@ private
 		end
 	end
 
-	def bike_check(data)
+	def bike_check(data)												#check the station is empty by checking the station's bemp
 		f = 1
 		 data_arr = data['retVal'].values
 		 data_arr.each do |bike|
@@ -105,32 +99,32 @@ private
 		h = {"code"=> code, "result"=>data}
 	end
 
-	def get_near_station(station)
-		dis_uri = "https://maps.googleapis.com/maps/api/distancematrix/json?"
-		dis_origin = "origins=#{params["lat"]},#{params["lng"]}"
-		api_key = "key=AIzaSyBaWsuNSpmcPIOzlfC9oR_R6HXvQ4qFyDo"
-		dis_set = "mode=walking&units=metric"
+	def get_near_station(station)												#find the two nearest station with bikes from the appointed location
+		dis_uri = "https://maps.googleapis.com/maps/api/distancematrix/json?"	#API URI
+		dis_origin = "origins=#{params["lat"]},#{params["lng"]}"				#origin and destination
+		api_key = "key=AIzaSyBaWsuNSpmcPIOzlfC9oR_R6HXvQ4qFyDo"					#API key
+		dis_set = "mode=walking&units=metric"									#API setting with walking mode and unit is m/km
 		valid_station_arr = []
 		station['retVal'].values.each do |bike_stat|
 			lat_dif = bike_stat["lat"].to_f - params["lat"].to_f
 			lng_dif = bike_stat["lng"].to_f - params["lng"].to_f
-				if bike_stat["bemp"].to_i != 0 &&  lat_dif.abs < 0.01 && lng_dif.abs < 0.01
+				if bike_stat["bemp"].to_i != 0 &&  lat_dif.abs < 0.01 && lng_dif.abs < 0.01	#select the station from the location in area with difference of 1 deg lat and lng
 					dis_path = dis_uri + dis_origin + "&destinations=" + bike_stat["lat"] + "," + bike_stat["lng"] + "&" + dis_set + "&" + api_key
 					
 					begin
-					get_distance = HTTParty::get(dis_path)
-					rescue HTTParty::Error => e
+					get_distance = HTTParty::get(dis_path)i						#for each selected station get its path distance-data from location
+					rescue HTTParty::Error => e									#handlde system error
 						return -3
 					rescue StandardError => e
 						return -3
 					end
 					distance_data = JSON.parse(get_distance.body)
-					distance = distance_data["rows"][0]["elements"][0]["distance"]["value"] 
-					valid_station_arr << {"sno" => bike_stat["sno"],"station" => bike_stat["sna"], "num_ubike" => bike_stat["sbi"], "dis" => distance}
+					distance = distance_data["rows"][0]["elements"][0]["distance"]["value"]	#get distance
+					valid_station_arr << {"sno" => bike_stat["sno"],"station" => bike_stat["sna"], "num_ubike" => bike_stat["sbi"], "dis" => distance}	#store distance and other info in hash arr
 			end
 		end
-		valid_station_arr = valid_station_arr.sort_by!{|h| h["dis"]}
-		h1 = {"station" => valid_station_arr[0]["station"], "num_bike" => valid_station_arr[0]["num_ubike"]}
+		valid_station_arr = valid_station_arr.sort_by!{|h| h["dis"]}			#sort the hash array by distance from near to far
+		h1 = {"station" => valid_station_arr[0]["station"], "num_bike" => valid_station_arr[0]["num_ubike"]}	#get two stantion data with re-pack to hash
 		h2 = {"station" => valid_station_arr[1]["station"], "num_bike" => valid_station_arr[1]["num_ubike"]}
 		return [h1, h2]
 	end
